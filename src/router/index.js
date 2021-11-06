feda07494efef1f46a7a122d389281eef520f15d
elenakout <elenakout@gmail.com>
2021-11-06 18:55:36 +0200

    create nested objects in json

--- src/router/index.js
+++ src/router/index.js
@@ -33,36 +33,3 @@ router.beforeEach((to, from, next) => {
 });
 
 export default router;
-
-// {
-//   path: "/:lang",
-//   component: RouterView,
-//   beforeEnter(to, from, next) {
-//     const lang = to.params.lang;
-//     if (!["en", "de"].includes(lang)) return next("de");
-//     if (i18n.locale !== lang) {
-//       i18n.locale = lang;
-//     }
-//     return next();
-//   },
-//   children: [
-//     {
-//       path: "home",
-//       name: "home",
-//       component: Home
-//     },
-//     {
-//       path: "about",
-//       name: "about",
-// route level code-splitting
-// this generates a separate chunk (about.[hash].js) for this route
-// which is lazy-loaded when the route is visited.
-//       component: () =>
-//         import(/* webpackChunkName: "about" */ "./views/About.vue")
-//     }
-//   ]
-// },
-// {
-//   path: "*",
-//   redirect: "/de"
-// }
